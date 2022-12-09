5b103da24ea8865e067c862fcf8c52b172772f6b
Carvalho <dcarvalho707@headmind.com>
2022-12-09 09:28:08 -0300

    mais testes

---
+++ projeto-teste/src/main/java/br/com/dcarvalho/projetoteste/ProjetoTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.dcarvalho.projetoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteApplication.class, args);
+	}
+
+}
---
+++ teste-kotlin/src/teste.kt
@@ -0,0 +1,3 @@
+fun teste(){
+    print("Hello World")
+}
